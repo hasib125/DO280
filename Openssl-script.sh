6b1fdf3668f6dba0921cf10d54b0c8618c3e275c
hasib125 <38103058+hasib125@users.noreply.github.com>
2026-04-17 01:57:51 +0600

    Update Openssl-script.sh

--- Openssl-script.sh
+++ Openssl-script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-printf "Country Name:"
-read Country
-printf "State or Province Name:"
-read ST
-printf "Locality:"
-read locality
-printf "Organization Name:"
-read OName
-printf "Organization Unit Name:"
-read OUName
-printf "MyName:"
-read Myname
-printf "Common Name:"
-read CName
+
+read -p "Country Name: " Country
+read -p "State or Province Name: " ST
+read -p "Locality: " locality
+read -p "Organization Name: " OName
+read -p "Organization Unit Name: " OUName
+read -p "My Name: " Myname
+read -p "Common Name: " CName
+
 openssl genrsa -out server.key 4096
-openssl req -new -key server.key -out server.csr -subj "/C=$Country/ST=$ST/L=$locality/O=$OName/OU=$OUName/CN=$CName"
+
+openssl req -new -key server.key -out server.csr \
+-subj "/C=$Country/ST=$ST/L=$locality/O=$OName/OU=$OUName/CN=$CName"
+
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
